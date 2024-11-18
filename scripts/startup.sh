66d7c805833bdadc40e2b91eb6c5c454521e39ed
Tony Zaky <tonyzaky.s@gmail.com>
2024-11-18 19:28:50 +0000

    Updated the startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,6 +3,7 @@
 # This script is used to start the ROS project. 
 
 # Build the workspace
+cd /bot-hoven-ros
 colcon build
 
 # Source the workspace setup file
@@ -13,3 +14,6 @@ ros2 launch finger_control finger_control.launch.py > log/launch.log 2>&1 &
 
 # Launch the hand_control nodes
 ros2 launch hand_control hand_control.launch.py > log/launch.log 2>&1 &
+
+# Launch the home_position nodes
+ros2 launch home_position home_position.launch.py > log/launch.log 2>&1 &
