f0dd567df9e216889269b761b97b5f3a5075f9a9
Tony Zaky <tonyzaky.s@gmail.com>
2024-11-18 19:28:13 +0000

    Updated the setup functions

--- src/finger_control/setup.py
+++ src/finger_control/setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name + '/launch', ['launch/finger_control.launch.py']),
+        ('share/' + package_name + '/launch', [f'launch/{package_name}.launch.py']),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
--- src/home_position/setup.py
+++ src/home_position/setup.py
@@ -7,8 +7,8 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name + '/launch', [f'launch/{package_name}.launch.py']),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
