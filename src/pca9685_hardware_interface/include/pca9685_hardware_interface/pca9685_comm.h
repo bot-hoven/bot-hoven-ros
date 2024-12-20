5c233231a40117c979066cc5f7780d4fe3a702fd
DobrowneyT <dobrownt@myumanitoba.ca>
2024-12-20 03:55:59 -0600

    -Modified the PCA9685 package to take the i2c address from the .xacro and open the bus from there. -Added the on_configure and on_cleanup methods

---
+++ src/pca9685_hardware_interface/include/pca9685_hardware_interface/pca9685_comm.h
@@ -0,0 +1,45 @@
+#ifndef RPY_PWM_HAT_PCA9685_H
+#define RPY_PWM_HAT_PCA9685_H
+
+#include <string>
+#include <memory>
+
+namespace PiPCA9685 {
+
+class I2CPeripheral;
+
+class PCA9685 {
+public:
+
+    PCA9685() = default;
+    ~PCA9685();
+
+    void connect_to_bus();
+    void disconnect();
+
+    void set_pwm_freq(const double freq_hz);
+
+    void set_pwm(const int channel, const uint16_t on, const uint16_t off);
+
+    void set_all_pwm(const uint16_t on, const uint16_t off);
+
+    void set_pwm_ms(const int channel, const double ms);
+
+    void set_device(const std::string &device_path) { device = device_path; }
+    void set_address(int addr) { address = addr; }
+    void set_frequency(int freq) { frequency = freq; }
+
+private:
+    std::unique_ptr<I2CPeripheral> i2c_dev;
+    
+    // Default frequency pulled from PCA9685 datasheet.
+    double frequency = 200.0;
+
+    const std::string &device = "/dev/i2c-1";
+    int address = 0x40;
+    int frequency = 50;
+};
+
+}  // namespace PiPCA9685
+
+#endif //RPY_PWM_HAT_PCA9685_H
