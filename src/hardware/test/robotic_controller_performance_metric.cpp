2d72f0a397c380b5bc02af3b15bee62d78fe257c
Tony Zaky <tonyzaky.s@gmail.com>
2025-02-04 14:38:02 -0600

    Setup Gtest for performance metric validation

---
+++ src/hardware/test/robotic_controller_performance_metric.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+TEST(robotic_controller_test, performance_metric)
+{
+  ASSERT_EQ(4, 2 + 2);
+}
+
+int main(int argc, char ** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
